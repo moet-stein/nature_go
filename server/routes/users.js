a1ab4a4818daf9371cca21429c44e1f102b4d035
Moe Steinmueller <0101.mste@gmail.com>
2021-06-09 10:39:03 +0200

    password with bcrypt done

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,6 +4,8 @@ const userModel = require('../models/usersModel');
 const catchAsync = require('../utils/catchAsync');
 const users = require('../controllers/users');
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
+const saltRounds = 12;
 
 router.get('/test', (req, res) => {
   res.send({ msg: 'Test route.' });
@@ -31,15 +33,17 @@ router.post(
   body('username').isLength({ min: 3 }),
   body('email').isEmail(),
   body('password').isLength({ min: 5 }),
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
+    const hash = await bcrypt.hash(req.body.password, saltRounds);
+
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
     const newUser = new userModel({
       username: req.body.username,
       email: req.body.email,
-      password: req.body.password,
+      password: hash,
     });
     newUser
       .save()
