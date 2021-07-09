ecf5d645b3da357f1e094d5d0916815ffa23de07
Moe Steinmueller <0101.mste@gmail.com>
2021-07-10 00:28:25 +0200

    fixed serverURL in config

--- client/src/config.js
+++ client/src/config.js
@@ -2,5 +2,5 @@ module.exports = {
   serverURL:
     process.env.NODE_ENV === 'development'
       ? 'http://localhost:5000/api'
-      : 'https://nature-go-app.herokuapp.com//api',
+      : 'https://nature-go-app.herokuapp.com/api',
 };
