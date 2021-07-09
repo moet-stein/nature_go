a4c3178ce5d7e63fd14f48cdf073d6f511baf827
Moe Steinmueller <0101.mste@gmail.com>
2021-07-09 21:45:29 +0200

    server.js line16 changed

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const { jwtStrategy } = require('./passport');
 
 // initialize express app
 const app = express();
-const port = process.env.PORT || 5000;
+const port = `${process.env.PORT}/api` || 5000;
 // https://nature-go-app.herokuapp.com/
 
 // connect to DB
